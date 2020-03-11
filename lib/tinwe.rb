96b1f381be66ba6547f985b668c990f0a46832ea
Martin Mihaylov <namel3ssone@gmail.com>
2020-03-11 23:03:25 +0200

    Add basic app structure

---
+++ lib/tinwe.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# This can be used to see statistics about your Lightroom Catalog.
+#
+# For a list on how to configure and use this, see:
+#
+# * `tinwe --help`
+module Tinwe
+  VERSION = '0.0.1'
+  HOME = 'https://github.com/namel3ss/tinwe'
+
+  require_relative 'tinwe/command'
+end
